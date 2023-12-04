3942592086c3531d1a2e876f1aaa67ef42dd4a0c
unknown <fethisert0@gmail.com>
2023-12-04 16:29:02 +0200

    kednime gelmem lazim :)

--- my-app/src/componnets/user_input.js
+++ my-app/src/componnets/user_input.js
@@ -1,12 +1,38 @@
-function UserInput(){
+import { memeData } from "../data/memeData";
+import React from "react";
+
+function UserInput(probs){
+
+    //BODY of Function
+
+    const [image, setImage] = React.useState(memeData[getRandomNum()].url)
+    const [buttonName, setName] = React.useState('get a new meme imagez');
+
+    function changeButtonName (){
+        setName('get a new memez hehe')
+    }
+    function getRandomNum(){
+        return Math.floor(Math.random() * (memeData.length));
+    }
+
+    function getRandomImage(){
+        setImage(memeData[getRandomNum()].url);
+    }
+
+   
     return (
         <div className='user-input-container'>
             <div className='input-group'>
-                <input placeholder="Shut up" type="text"></input>
-                <input placeholder="And take my money" type="text"></input>
+                <input placeholder="Top text" type="text"></input>
+                <input placeholder="Bottom text" type="text"></input>
             </div>
 
-            <button>Get a new meme image</button>
+            <button onClick={()=>{
+                getRandomImage()
+                changeButtonName()
+            }}>{ buttonName }</button>
+
+            <img src={ image }></img>
         </div>
     );
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,18 +4,88 @@ import './index.css';
 import Header from './componnets/header';
 import UserInput from './componnets/user_input';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
 
 
-
 function App(){
+
+  let isYes = true;
+  const [title, setTitle] = React.useState("0");
+  
+  function increase(){
+    const strNum = title;
+    const intNum = parseInt(title) + 1;
+    setTitle(intNum);
+  }
+
+  function decrease(){
+    const strNum = title;
+    if(title != '0'){
+      const intNum = parseInt(title) - 1;
+      setTitle(intNum)
+    }else{
+      alert('I cant go to negative numbers')
+    }
+   
+  }
+  
+  
+  
   return (
     <div className='app-container'>
-        <Header></Header>
-        <UserInput />
+      <div className='state'>
+        <div className='state-value'>
+          <h1  className='title'> { title } </h1>
+        </div>
+        <button onClick={ decrease } className='button-left'>-</button>
+        <button onClick={ increase } className='button-right'>+</button>
+      </div>
     </div>
   );
 }
+
+
+
+
+
+  {/* <Header></Header>
+      <UserInput/>  */}
+
+// function greetings(name){
+
+//   const date = new Date();
+//   const hours = date.getHours();
+
+//   let timeOfDay;
+//   console.log(hours);
+//   if(hours >= 4 && hours < 12){
+//     timeOfDay = 'morning';
+//   }else if(hours >= 12 && hours < 17){
+//     timeOfDay = 'afternoon';
+//   }else if(hours >= 17 && hours < 20){
+//     timeOfDay = 'evening';
+//   }else{
+//     timeOfDay = 'night';
+//   }
+
+//   return `Good ${timeOfDay} ${name}`;
+
+  
+// }
+
+// const greetingMsg = greetings('Fethi')
+// console.log(greetingMsg);
+
+// setThings(prevState => {
+//   console.log(prevState);
+//   return [...prevState, newThingElement]
+// });
+
+
+   {/* <Header></Header>
+        <UserInput /> 
+        */}
