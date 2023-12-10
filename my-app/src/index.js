99ac0fbfda0dc15ff4bf149ae68fb700bf3c001a
fethi <fethisert0@gmail.com>
2023-12-10 13:28:07 +0200

    my-form

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,6 +16,8 @@ root.render(
 
 function App(props){
  
+  
+
   return (
     <div className='app-container'>
        <Form />
@@ -28,78 +30,46 @@ function Form(){
   //With putting user data in state eveery updating to state with onChange event we don't need take data at the end with state stle when I press
   //The submit i already have to data
   
-  const [inputs, setInput] = useState({ name: '', surname: '', age: '', banana: false , favcolor: ''})
+  const [userState, setUserState] = useState({email: '', password: '', passwordConfirm: '', isNewSteller : false});
 
-  function handleInputs(e){
-      console.log(e);
-      const {name, type, value, checked} = e.target;
-      setInput(prevState => ({...prevState, [name] : type === 'checkbox' ? checked : value })); 
+  function handleInput(event){
+    const {name , type , value, checked } = event.target;
+    setUserState(prevState => ({...prevState,[name]: type === 'checkbox' ? checked : value}))
   }
 
-  function handleSubmit(e){
-    e.preventDefault();
-    apiFunction(inputs);
-    //if I'dont preventDefault form onSubmit event will refresh to page it's not necessary
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(userState.password !== userState.passwordConfirm){
+        alert('Passwords shuld match braza');
+        return;
+    }
+    apiPost(userState);
   }
 
-  function apiFunction(data){
-    console.log(data);
+  const apiPost = (stateData) => {
+    console.log(stateData);
   }
-
-  console.log(inputs);
-
   return (
-    <form onSubmit = { handleSubmit }>
-     
-      <input type='text' name='name' value={ inputs.name } onChange={ handleInputs} placeholder='Name'/>
-      <input type='text' name='surname' value={ inputs.surname } onChange={ handleInputs} placeholder='Surname'/>
-      <input type='text' name='age' value={ inputs.age } onChange={ handleInputs}  placeholder='Age' />
+    <form onSubmit={ handleSubmit }>
+      <input type='text' value={ userState.email } name='email'  onChange={ handleInput} placeholder='Email' required/>
+      <input type='password' value={ userState.password } name='password' onChange={ handleInput} placeholder='Password' required/>
+      <input type='password' value={ userState.passwordConfirm } name='passwordConfirm'  onChange={ handleInput} placeholder='ConfirmPassword' required/>
 
-      <Checkbox label = 'Bananaz' name='banana' checked = { inputs.banana } onChange = { handleInputs }/>
+      <label>
+        <input type='checkbox' checked={ userState.isNewSteller } onChange={ handleInput } name='isNewSteller'/>
+        I want to join the newsletter
+      </label>
 
-      <fieldset>
-        <legend>Current employement status</legend>
-        <Radiobox label={ 'Unemployed'} id={ 'unemployed'}  value={ 'unemployed' } onChange = { handleInputs }  name={ 'employment' } stateData = {inputs.employment}/>
-        <Radiobox label={ 'Part-time' } id={ 'parttime' }  value={ 'parttime' } onChange = { handleInputs } name={ 'employment' } stateData = {inputs.employment}/>
-        <Radiobox label={ 'Full-time' } id={ 'fulltime' }  value={ 'fulltime' } onChange = { handleInputs } name={ 'employment' } stateData = {inputs.employment}/>
-      </fieldset>
-
-      <label htmlFor='favColor'>What is your favorite color?</label>
-      <select name='favcolor' id='favColor' value={ inputs.favcolor } onChange={ handleInputs }>
-        <option value={''}>-- Choose --</option>
-        <option value={'Red'}>Red</option>
-        <option value={'Blue'}>Blue</option>
-        <option value={'Green'}>Green</option>
-        <option value={'Yellow'}>Yellow</option>
-        <option value={'Purple'}>Purple</option>
-        <option value={'Orange'}>Orange</option>
-      </select>
-      {/* <input type='submit' value='Submit'></input> */}
-      <button>Submit</button> 
-      {/* button element form elementinin childerini oldugu icin default olarak type submittir button kullanmak daha iyi
-          cunku input type arti value vermek gereksiz value ayni zamanda button texti oluyor degeri valusu degil*/}
+      <button>Submit</button>
     </form>
   );
+
+
 }
 
-//State of htmlelement shuold come from state in jsx not html state
-// radio elementinde namelerin ayni olmasi tek bir radionun sadece aktif kalmasini saglar eger ayni name attirbutesini tutan 3 radio varsa sadece biri aktif kalabilir
-const Radiobox = ( {label, id, onChange, name, value, stateData}) => {
-  return (
-    <label>
-      <input type = 'radio' id = { id }  onChange = { onChange } name = { name } value={ value } checked={ stateData === value } ></input> 
-      {label}
-    </label>
-  );
-}
 
-const Checkbox = ( { label , checked , onChange, name  } ) => {
-  return (
-    <label>
-      <input type='checkbox' checked = { checked }  onChange = { onChange } name = { name }/>
-      {label}
-    </label>
-  );
-}
+
+
+
 {/* <Header />
 <UserInput /> */}
